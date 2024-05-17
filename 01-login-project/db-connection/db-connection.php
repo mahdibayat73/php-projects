e8b23c3f63da6992af384d62fb2067ffc8a2f843
mahdibayat73 <mahdi.riraco@gmail.com>
2024-05-17 17:42:41 +0330

    Create Database Connection File

--- 01-login-project/db-connection/db-connection.php
+++ 01-login-project/db-connection/db-connection.php
@@ -1,7 +1,14 @@
 <?php 
 
 $servername = "localhost";
-$username = "root";
-$password = "root";
+$dbusername = "root";
+$dbpassword = "root";
 $dbname = "users01";
 
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+//Check connection
+if ( $conn->connect_error ) {
+    die("Connection Failed: " . $conn->connect_error);
+}
