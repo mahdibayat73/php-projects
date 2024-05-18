18aa64623b88fd78c949ccb79259ade93e268845
mahdibayat73 <mahdi.riraco@gmail.com>
2024-05-18 18:06:58 +0330

    Checking fields are not empty

--- 01-login-project/forms/inc/login.php
+++ 01-login-project/forms/inc/login.php
@@ -7,3 +7,20 @@ function validate_input($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
+
+if ( $_SERVER["REQUEST_MOTHOD"] == "POST" ) {
+    $userEmail = validate_input($_POST["useremail"]);
+    $userPass = validate_input($_POST["userpass"]);
+    // Array for error handling
+    $errors = [];
+
+    // Checking fields are not empty
+    if ( empty($userEmail) || empty($userPass) ) {
+        array_push($errors, "All fields are required!");
+    }
+    if ( !filter_var($userEmail, FILTER_VALIDATE_EMAIL) ) {
+        array_push($errors, "Invalid email format!");
+    }
+
+
+}
