308474497bbf0b4f4284af4afb02de43fa05b51d
mahdibayat73 <mahdi.riraco@gmail.com>
2024-05-18 19:42:31 +0330

    Create logout.php file and add a logout button in index page

---
+++ 01-login-project/forms/inc/logout.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page
+header("Location: ../login-register.php");
--- 01-login-project/index.php
+++ 01-login-project/index.php
@@ -1,6 +1,22 @@
 <?php 
 session_start();
+
+// Check if the user is logged in
 if ( !isset($_SESSION["username"]) ) {
     header("Location: forms/login-register.php");
+    exit();
 }
-echo "This is doshbord page";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+    <form action="forms/inc/logout.php" method="POST">
+        <input type="submit" value="Logout">
+    </form>
+</body>
+</html>
