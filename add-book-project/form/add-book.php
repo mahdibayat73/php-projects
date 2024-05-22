15c8b773a3e426e6ab4d0442e96a79375d929f14
mahdibayat73 <mahdi.riraco@gmail.com>
2024-05-22 21:07:05 +0330

    Set errors for each inputs

--- add-book-project/form/add-book.php
+++ add-book-project/form/add-book.php
@@ -1,8 +1,10 @@
-<?php 
+<?php
+session_start();
 require_once("../loader/db-connection.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,6 +12,7 @@ require_once("../loader/db-connection.php");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Add Book</title>
 </head>
+
 <body>
     <div class="container">
         <header class="d-flex justify-content-between my-4">
@@ -18,24 +21,42 @@ require_once("../loader/db-connection.php");
                 <a class="btn btn-primary" href="#">Back</a>
             </div>
         </header>
-        <form action="process.php" method="post">
+        <?php
+        // Get values from the query string if present
+        if (isset($_SESSION['form_data'])) {
+            $title = htmlspecialchars($_SESSION['form_data']['title']);
+            $author = htmlspecialchars($_SESSION['form_data']['author']);
+            $type = htmlspecialchars($_SESSION['form_data']['type']);
+            $description = htmlspecialchars($_SESSION['form_data']['description']);
+            unset($_SESSION['form_data']);
+        }
+        ?>
+        <?php if (isset($_SESSION["errors"]) && !empty($_SESSION["errors"])) : ?>
+            <div class="alert alert-danger" role="alert">
+                <?php foreach ($_SESSION["errors"] as $error) : ?>
+                    <?php echo $error . "<br>"; ?>
+                <?php endforeach; ?>
+            </div>
+            <?php unset($_SESSION["errors"]); ?>
+        <?php endif; ?>
+        <form action="../inc/process.php" method="post">
             <div class="form-element my-4">
-                <input class="form-control" type="text" name="title" placeholder="Book Title:">
+                <input class="form-control" type="text" name="title" placeholder="Book Title:" value="<?php echo $title; ?>">
             </div>
             <div class="form-element my-4">
-                <input class="form-control" type="text" name="title" placeholder="Author Name:">
+                <input class="form-control" type="text" name="author" placeholder="Author Name:" value="<?php echo $author; ?>">
             </div>
             <div class="form-element my-4">
                 <select class="form-control" name="type">
                     <option value="">Select Book Type</option>
-                    <option value="Adventure">Adventure</option>
-                    <option value="Fantast">Fantast</option>
-                    <option value="SciFi">SciFi</option>
-                    <option value="Horror">Horror</option>
+                    <option value="Adventure" <?php echo $type == 'Adventure' ? 'selected' : ''; ?> >Adventure</option>
+                    <option value="Fantast" <?php echo $type == 'Fantast' ? 'selected' : ''; ?> >Fantast</option>
+                    <option value="SciFi" <?php echo $type == 'SciFi' ? 'selected' : ''; ?>>SciFi</option>
+                    <option value="Horror" <?php echo $type == 'Horror' ? 'selected' : ''; ?> >Horror</option>
                 </select>
             </div>
             <div class="form-element my-4">
-                <input class="form-control" type="text" name="desc" placeholder="Book Desctiption:">
+                <input class="form-control" type="text" name="desc" placeholder="Book Desctiption:" value="<?php echo $description; ?>">
             </div>
             <div class="form-element">
                 <input class="btn btn-success" type="submit" name="create" value="Add Book">
@@ -43,4 +64,5 @@ require_once("../loader/db-connection.php");
         </form>
     </div>
 </body>
+
 </html>
---
+++ add-book-project/inc/process.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+
+// Cleaning malicious codes
+function validate_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data); // Note: changed from stripcslashes to stripslashes
+    $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+    return $data;
+}
+
+// Initialize variables with empty values
+$title = $author = $type = $description = "";
+
+// Errors collection
+$errors = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Initialize variables with empty values
+    $title = $author = $type = $description = "";
+
+    // Form information collection
+    $title = validate_input($_POST["title"]);
+    $author = validate_input($_POST["author"]);
+    $type = validate_input($_POST["type"]);
+    $desc = validate_input($_POST["desc"]);
+
+    // Collect all inputs into an array
+    $inputs = [
+        'title' => $title,
+        'author' => $author,
+        'type' => $type,
+        'description' => $desc
+    ];
+
+    // Check if all fields are filled
+    foreach ($inputs as $key => $value) {
+        if (empty($value)) {
+            array_push($errors, "All fields are required!");
+            break;
+        }
+    }
+
+    // Define patterns for each input except 'type'
+    $patterns = [
+        'title' => "/^[a-zA-Z0-9 ]*$/",
+        'author' => "/^[a-zA-Z0-9 ]*$/",
+        'description' => "/^[a-zA-Z0-9 ,.]*$/"
+    ];
+
+    // Validate inputs against their patterns, excluding 'type'
+    foreach ($inputs as $key => $value) {
+        if ($key !== 'type' && !preg_match($patterns[$key], $value)) {
+            array_push($errors, ucfirst($key) . " contains invalid characters.");
+        }
+    }
+
+    // Display errors (for debugging purposes)
+    if (!empty($errors)) {
+        $_SESSION["errors"] = $errors;
+        $_SESSION['form_data'] = $inputs;
+        header("Location: ../form/add-book.php");
+    } else {
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+    }
+}
