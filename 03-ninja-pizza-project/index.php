3e0e8e304cf703e701abde5eb34574304ebc6089
mahdibayat73 <mahdi.riraco@gmail.com>
2024-06-01 14:32:34 +0330

    make Dynamic Website Title Using PHP

--- 03-ninja-pizza-project/index.php
+++ 03-ninja-pizza-project/index.php
@@ -1,7 +1,10 @@
-<?php include_once "inc/header.php"; ?>
+<?php
+$title = "Home page";
+include_once "inc/header.php";
+?>
 
 <main class="container">
-    
+
 </main>
 
 <?php include_once "inc/footer.php"; ?>
