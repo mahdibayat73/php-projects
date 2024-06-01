96af350e6ee88976eb05bd02477d64db262c84ae
mahdibayat73 <mahdi.riraco@gmail.com>
2024-06-01 14:01:21 +0330

    Completed header.php file

--- 03-ninja-pizza-project/inc/header.php
+++ 03-ninja-pizza-project/inc/header.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
     <title>Document</title>
 </head>
-<body>
+
+<body class="bg-light">
+
+    <header id="header" class="bg-white">
+        <div class="container">
+            <div class="items-wrapper d-flex justify-content-between align-items-center py-4">
+                <div class="logo">Ninja Pizza</div>
+                <a href="#" class="add-pizza btn btn-primary">Add Pizza</a>
+            </div>
+        </div>
+    </header>
