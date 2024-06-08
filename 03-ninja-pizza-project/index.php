2e83c158605834c9b6fbb26b6e3a3ef3a76fcf1a
mahdibayat73 <mahdi.riraco@gmail.com>
2024-06-08 18:20:33 +0330

    complete show card in index page

--- 03-ninja-pizza-project/index.php
+++ 03-ninja-pizza-project/index.php
@@ -12,6 +12,44 @@ include_once "inc/header.php";
             <?php unset($_SESSION["added"]); ?>
         </div>
     <?php endif; ?>
+    <div class="row my-4">
+        <div class="col-12 col-lg-4 col-md-6 p-2">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link btn btn-primary">Card link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 p-2">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link btn btn-primary">Card link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 p-2">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link btn btn-primary">Card link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 p-2">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link btn btn-primary">Card link</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php include_once "inc/footer.php"; ?>
