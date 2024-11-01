7a3d444ec0f8f116103adf36c8d1a5ca1d00a56f
mahdibayat73 <mahdi.riraco@gmail.com>
2024-11-01 20:36:24 +0330

    Create a connection to Users01

---
+++ 02-pdo-connection/db-connection/13-connection.php
@@ -0,0 +1,16 @@
+<?php 
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "root";
+$dbname = "users01";
+
+// Create and check connection
+
+try {
+    $conn = new PDO("mysql:dbhost=$dbhost; dbname=$dbname", $dbuser, $dbpass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch ( PDOException $e ) {
+    echo "Connection failed: " . $e->getMessage();
+}
